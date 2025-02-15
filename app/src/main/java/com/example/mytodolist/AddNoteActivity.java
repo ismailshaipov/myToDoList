80257b24697fda1ee0c7c56d187bcbd9d39bbc18
ismailsaipov <ismail_shaipov95@mail.ru>
2025-02-16 01:49:33 +0300

    ViewHolder

---
+++ app/src/main/java/com/example/mytodolist/AddNoteActivity.java
@@ -0,0 +1,84 @@
+package com.example.mytodolist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
+    private RadioButton radioBtnLow;
+    private RadioButton radioBtnMedium;
+    private RadioButton radioBtnHigh;
+    private Button btnSave;
+
+    private Database database = Database.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initViews();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextNote = findViewById(R.id.editTextNote);
+        radioBtnLow = findViewById(R.id.radioBtnLow);
+        radioBtnMedium = findViewById(R.id.radioBtnMedium);
+        radioBtnHigh = findViewById(R.id.radioBtnHigh);
+        btnSave = findViewById(R.id.btnSave);
+    }
+
+    private void saveNote() {
+        String text = editTextNote.getText().toString().trim();
+        int priority = getPriority();
+        int id = database.getNotes().size();
+        Note note = new Note(id,text,priority);
+        database.add(note);
+
+        finish();
+
+
+    }
+
+    private int getPriority() {
+        int priority;
+        if (radioBtnLow.isChecked()) {
+            priority = 0;
+        }else if(radioBtnMedium.isChecked()){
+            priority = 1;
+        } else {
+            priority = 2;
+        }
+        return priority;
+
+    }
+
+    public static Intent newIntent(Context context){
+        return new Intent(context,AddNoteActivity.class);
+    }
+}
---
+++ app/src/main/java/com/example/mytodolist/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.mytodolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerviewNotes;
+    private FloatingActionButton buttonAddNote;
+
+    private Database database = Database.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initViews();
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showNotes();
+    }
+
+    private void initViews() {
+        recyclerviewNotes = findViewById(R.id.recyclerviewNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        linearLayoutNotes.removeAllViews();
+        for (Note note : database.getNotes()) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.note_item,
+                    linearLayoutNotes,
+                    false);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    database.remove(note.getId());
+                    showNotes();
+                }
+            });
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+            }
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+            linearLayoutNotes.addView(view);
+        }
+    }
+}
